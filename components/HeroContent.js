cc3ee867049601350a6e07772736ea9a879c70bc
Jackson Kaufman <jacksonkaufman115@gmail.com>
2022-04-13 17:33:14 -0600

    componentized hero content and added various global styles

---
+++ components/HeroContent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HeroContent() {
+  return (
+    <div className="min-h-screen pt-16 pb-16 flex flex-1 flex-col justify-center items-center">
+      <h1 className="z-20">
+        Camp Goofball 2022!
+      </h1>
+    </div>
+  )
+}
+
+export default HeroContent;
