8e6f4517f1f88cf5fcd60e890be0a66332b8cd49
Jackson Kaufman <jacksonkaufman115@gmail.com>
2022-04-13 17:02:36 -0600

    added proper images, cleaned up slider component

--- components/heroSlider.js
+++ components/heroSlider.js
@@ -1,15 +1,42 @@
 import React, { useEffect } from 'react'
 import Image from 'next/image'
-import goofGroup from '../assets/images/GoofGroup1.jpg'
-import groupOne from '../assets/images/group.jpg'
-import groupThree from '../assets/images/TSgroup3.jpg'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
 
 
-
-export default function HeroSlider() {
-  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, speed: 5 }, [Autoplay({delay: 7000})])
+function HeroSlider() {
+    const photos = [
+    {
+      label: 'Midnight',
+      path: '/images/heroSlider/1-Midnight.jpg',
+    },
+    {
+      label: 'Crowd',
+      path: '/images/heroSlider/2-Crowd.jpg',
+    },
+    {
+      label: 'Steve',
+      path: '/images/heroSlider/3-Steve.jpg',
+    },
+    {
+      label: 'Amac',
+      path: '/images/heroSlider/4-Amac.jpg',
+    },
+    {
+      label: 'Group Stage',
+      path: '/images/heroSlider/5-GroupStage.jpg',
+    },
+    {
+      label: 'Massif',
+      path: '/images/heroSlider/6-Massif.jpg',
+    },
+  ];
+  const carouselOptions = {
+    loop: true,
+    speed: 5,
+    draggable: false
+  }
+  const [emblaRef, emblaApi] = useEmblaCarousel(carouselOptions, [Autoplay({delay: 7000})])
   useEffect(() => {
   if (emblaApi) {
     // Embla API is ready
@@ -19,37 +46,21 @@ export default function HeroSlider() {
   return (
     <div className="embla absolute w-full h-full z-10" ref={emblaRef}>
       <div className="embla__container w-full h-full">
-        <div className="embla__slide h-full w-full">
-          <Image
-            className="slider-image"
-            src={goofGroup}
-            alt="Group Goof"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </div>
-        <div className="embla__slide">
-          <Image
-            className="slider-image"
-            src={groupOne}
-            alt="Group Goof"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </div>
-        <div className="embla__slide">
-          <Image
-            className="slider-image"
-            src={groupThree}
-            alt="Group Goof"
-            layout="fill"
-            objectFit="cover"
-            objectPosition="center"
-          />
-        </div>
+        {photos.map((photo) => (
+          <div className="embla__slide h-full w-full" key={photo.label}>
+            <Image
+              className="slider-image"
+              src={photo.path}
+              alt={photo.label}
+              layout="fill"
+              objectFit="cover"
+              objectPosition="center"
+            />
+          </div>
+        ))}
       </div>
     </div>
   )
 }
+
+export default HeroSlider;
