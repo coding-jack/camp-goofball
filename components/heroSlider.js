76288b0aa2ca87ee74035c7e01059f2f96289a1b
Jackson Kaufman <jacksonkaufman115@gmail.com>
2022-04-14 14:26:42 -0600

    removed embla carousel from dependencies

--- components/heroSlider.js
+++ components/heroSlider.js
@@ -1,12 +1,10 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import Image from 'next/image'
-// import useEmblaCarousel from 'embla-carousel-react'
-// import Autoplay from 'embla-carousel-autoplay'
 import Slider from 'react-slick'
 
 
 function HeroSlider() {
-    const photos = [
+  const photos = [
     {
       label: 'Midnight',
       path: '/images/heroSlider/1-Midnight.jpg',
@@ -31,7 +29,7 @@ function HeroSlider() {
       label: 'Massif',
       path: '/images/heroSlider/6-Massif.jpg',
     },
-    ];
+  ];
   const settings = {
     dots: false,
     autoplay: true,
@@ -44,35 +42,8 @@ function HeroSlider() {
     cssEase: 'linear',
     slidesToScroll: 1
   }
-  // const carouselOptions = {
-  //   loop: true,
-  //   speed: 5,
-  //   draggable: false
-  // }
-  // const [emblaRef, emblaApi] = useEmblaCarousel(carouselOptions, [Autoplay({delay: 7000})])
-  // useEffect(() => {
-  // if (emblaApi) {
-  //   // Embla API is ready
-  // }
-  // }, [emblaApi])
 
   return (
-    // <div className="embla absolute w-full h-full z-10" ref={emblaRef}>
-    //   <div className="embla__container w-full h-full">
-        // {photos.map((photo) => (
-        //   <div className="embla__slide h-full w-full" key={photo.label}>
-        //     <Image
-        //       className="slider-image"
-        //       src={photo.path}
-        //       alt={photo.label}
-        //       layout="fill"
-        //       objectFit="cover"
-        //       objectPosition="center"
-        //     />
-        //   </div>
-        // ))}
-    //   </div>
-    // </div>
     <div className="slider absolute w-full h-full z-10">
       <Slider {...settings}>
         {photos.map((photo) => (
