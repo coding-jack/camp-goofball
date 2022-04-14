68d58947251ad71f97094e4d0b38e7849c4750bc
Jackson Kaufman <jacksonkaufman115@gmail.com>
2022-04-14 15:08:25 -0600

    Added typeface franchise globally

--- components/HeroContent.js
+++ components/HeroContent.js
@@ -5,7 +5,7 @@ function HeroContent() {
   return (
     <div className="min-h-screen pt-16 pb-16 flex flex-1 flex-col justify-center items-center">
       <Image className="z-20" src="/placeholderIcon.png" alt="Camp Logo" width={172} height={160} />
-      <h1 className="subheader z-20 text-white">
+      <h1 className="subheader z-20 text-white text-5xl">
         Camp Goofball 2022
       </h1>
     </div>
