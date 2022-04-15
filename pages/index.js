d647acfd74c29189048ad20fd5034a91d3cee19d
Jackson Kaufman <jacksonkaufman115@gmail.com>
2022-04-15 13:21:29 -0600

    testing netlify build

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
-import HeroSlider from '../components/HeroSlider'
 import HeroContent from '../components/HeroContent'
+import HeroSlider from '../components/HeroSlider'
 import MailingListSignup from '../components/NewsletterForm'
 
 export default function Home() {
